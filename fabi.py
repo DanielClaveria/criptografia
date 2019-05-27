34c3fce00614564f74d5f47399cfb8098486795a
daniel claveria <daniel.claveria@gmail.com>
2019-05-27 19:16:06 -0400

    dcs

---
+++ fabi.py
@@ -0,0 +1,13 @@
+x = input('¿hasta cuanto quieres contar?\n ingrese su respuesta: ')
+
+print(x)
+
+i = 1
+while (i <= int(x)):
+    i = i+1
+
+print ('listo, termine, ahora i vale:')
+print (i)
+
+
+
---
+++ testmodulos.py
@@ -0,0 +1,3 @@
+import fuerza_bruta
+objFuerzaBruta = FuerzaBrutaHash(in_tipo_hash,txt_hash,rango_caracteres)
+objFuerzaBruta.combineChars(prefijo,1,maximo)
